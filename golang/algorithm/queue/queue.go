a7e0bb7c904af7aef6157666ce4272ee0a294bb4
zhaoshihao <zhaoshihao@sensorsdata.com>
2024-02-21 16:29:52 +0800

    确定是测试用例的问题了，和官方4ms的思路一模一样，用4ms的代码跑，最后也是10ms，就这样了

--- golang/algorithm/queue/queue.go
+++ golang/algorithm/queue/queue.go
@@ -3,7 +3,7 @@ package main
 type Queue struct {
 	arr  []int
 	head int
-	tail int
+	// tail int
 	cap  int
 	size int
 }
@@ -25,10 +25,6 @@ func (q *Queue) Pop() bool {
 	if q.Isempty() {
 		return false
 	} else {
-		// q.head++
-		// if q.head == q.cap {
-		// 	q.head = 0
-		// }
 		q.head = (q.head + 1) % q.cap
 		q.size--
 		return true
@@ -36,27 +32,31 @@ func (q *Queue) Pop() bool {
 }
 
 func (q *Queue) Isempty() bool {
-	if q.size == 0 {
-		return true
-	} else {
-		return false
-	}
+	return q.size == 0
+	// if q.size == 0 {
+	// 	return true
+	// } else {
+	// 	return false
+	// }
+	// if else去掉 节约了很多时间
 }
 
 func (q *Queue) Isfull() bool {
-	if q.size == q.cap {
-		return true
-	} else {
-		return false
-	}
+	return q.size == q.cap
+	// if q.size == q.cap {
+	// 	return true
+	// } else {
+	// 	return false
+	// }
+	// if else去掉 节约了很多时间
 }
 
 func (q *Queue) Push(item int) bool {
 	if q.Isfull() {
 		return false
 	} else {
-		q.arr[q.tail] = item
-		q.tail = (q.tail + 1) % q.cap
+		index := (q.head + q.size) % q.cap
+		q.arr[index] = item
 		q.size++
 		return true
 	}
@@ -74,11 +74,13 @@ func (q *Queue) Rear() int {
 	if q.Isempty() {
 		return -1
 	} else {
-		if q.tail != 0 { // 这里需要考虑tail为0的情况
-			return q.arr[q.tail-1]
-		} else {
-			return q.arr[q.cap-1]
-		}
+		index := (q.head + q.size - 1) % q.cap
+		return q.arr[index]
+		// if q.tail != 0 { // 这里需要考虑tail为0的情况
+		// 	return q.arr[q.tail-1]
+		// } else {
+		// 	return q.arr[q.cap-1]
+		// }
 	}
 }
 
