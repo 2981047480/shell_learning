e5252880a7e34dbef69124e0276ecb043543b369
eleven <eleven@elevenMBP.lan>
2024-04-05 02:07:50 +0800

    配置文件上传 done

--- golang/k8s-manager/main.go
+++ golang/k8s-manager/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"example.com/m/v2/k8s-manager/handler/config"
 	"example.com/m/v2/k8s-manager/handler/node"
 	"example.com/m/v2/k8s-manager/handler/sys"
 	"github.com/gin-gonic/gin"
@@ -10,5 +11,6 @@ func main() {
 	r := gin.Default()
 	r.GET("/ping", sys.Ping)
 	r.GET("/node/list", node.List)
+	r.POST("/config/upload", config.Upload)
 	r.Run("127.0.0.1:8080") // 自己定义，不写默认是127.0.0.1:8080
 }
